f63c731a60aab685ab7ba6b55c5f160811727637
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-10-23 22:55:46 +0200

    Print emptystrdeque diagnostic message

--- strdequeconst.cc
+++ strdequeconst.cc
@@ -1,6 +1,18 @@
+#include <iostream>
 #include "strdequeconst.h"
 
+#ifndef NDEBUG
+	const static bool debug = true;
+#else
+	const static bool debug = false;
+#endif
+
+
 unsigned long emptystrdeque() {
+    if (debug) {
+        std::cerr << "emptystrdeque()" << std::endl;
+    }
+
     static unsigned long id = 0;
     return id;
 }
